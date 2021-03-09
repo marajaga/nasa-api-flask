3b548d13aa0e3b6a7fc5ed73e17b2facd7569050
Fanny <fanny.pringere@icloud.com>
2021-03-09 11:08:16 +0100

    modification

--- nasa.py
+++ nasa.py
@@ -7,11 +7,9 @@ nasa_api_key = os.getenv("NASA_API_KEY")
 nasa = nasapy.Nasa(key = nasa_api_key)
 
 def picture_of_the_day():
-    # Picture of the day
     d = datetime.now()
     print(d)
 
     #Get the image data:
-    #apod = nasa.picture_of_the_day(date=d, hd=True) 
     apod = nasa.picture_of_the_day(date=d, hd=True)     
     return apod
